3f323e2778c8540c0c3947de0e6b516c7dfc2c9c
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-20 16:06:09 -0300

    Add files via upload

---
+++ area_de_triangulo.py
@@ -0,0 +1,4 @@
+lado = float(input("Informe o lado do triangulo: "))
+altura = float(input("informe a altura do triangulo: "))
+area_triangulo = (lado * altura)/2
+print(f"O valor da area com os lado {lado} e altura {altura} é {area_triangulo}")
