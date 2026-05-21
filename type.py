0215f4b860c3be38723781ae704a4efc6daba8d7
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-21 19:59:08 -0300

    Update type.py

--- type.py
+++ type.py
@@ -1,3 +1,4 @@
+teste = int(input("Me diga o numero: "))
 nome = input("me diga seu nome por favor: ")
 n = input("me diga qualquer numero ai sabixao")
 tf = bool(input("Voce namora?"))
@@ -7,3 +8,9 @@ print (tf)
 print (type(nome))
 print (type(n))
 print (type(tf))
+if (teste == 1):
+    print("esse é o numero 1")
+elif (teste == 2):
+    print ("Esse é numero 2")
+else:
+    print("esse numero eu nao pego")
